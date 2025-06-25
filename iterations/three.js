35ae4008e584d8a918162a6fe09c5d3d951ab622
summu2801 <harshitkv.cs.23@nitj.ac.in>
2025-06-25 08:45:40 +0000

    learn about for of loop

--- iterations/three.js
+++ iterations/three.js
@@ -1,21 +1,40 @@
+// for of
 
-let index = 0
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
-// }
+// ["", "", ""]
+// [{}, {}, {}]
 
-let myArray = ['flash', "batman", "superman"]
+const arr = [1, 2, 3, 4, 5]
 
-let arr = 0
-while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
-    arr = arr + 1
+for (const num of arr) {
+    //console.log(num);
 }
 
-let score = 11
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
 
-do {
-    console.log(`Score is ${score}`);
-    score++
-} while (score <= 10);
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
